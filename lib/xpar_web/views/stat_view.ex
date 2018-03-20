d56eeb87a303e77cb7dbaaa493a79b6e977221bb
NAVEEN <nav3011186@gmail.com>
2018-03-20 07:46:30 +0100

    sort pairng data based on no of days paired together

--- lib/xpar_web/views/stat_view.ex
+++ lib/xpar_web/views/stat_view.ex
@@ -2,8 +2,7 @@ defmodule XparWeb.StatView do
   use XparWeb, :view
 
   def pairs(conn) do
-      stats = conn.assigns[:stats]
-      IO.inspect stats
-      stats
+    conn.assigns[:stats]
+     |> Enum.sort_by(fn stat -> stat.percent end)
   end
 end
