1b2affb62e22da5500091f50d80a1481a88960e0
NAVEEN <nav3011186@gmail.com>
2018-03-23 08:04:19 +0100

    adding git-crypred files

--- lib/xpar/client/bit_bucket.ex
+++ lib/xpar/client/bit_bucket.ex
@@ -1,9 +1,6 @@
 defmodule Xpar.Client.BitBucket do
   @bitbucket_url Application.get_env(:xpar, :base_url)
   def get_commit_messages(team, repo_name) do
-    IO.puts "**************************************************"
-    IO.inspect @bitbucket_url
-    IO.puts "**************************************************"
      response = HTTPoison.get! "#{@bitbucket_url}/#{team}/repos/#{repo_name}/commits/"
     {:ok, response_map}= Poison.decode(response.body)
     IO.inspect response_map
